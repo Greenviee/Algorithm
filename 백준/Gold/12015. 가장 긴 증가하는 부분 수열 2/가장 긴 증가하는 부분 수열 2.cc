d8ea5b2dc757b5b6bc1b25e856fd04561196a96a
Greenviee <131782814+Greenviee@users.noreply.github.com>
2023-07-27 23:14:13 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 496 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+int N;
+vector<int> arr;
+vector<int> answer;
+
+void LIS() {
+    for(int i=0;i<arr.size();i++) {
+        int n = arr[i];
+        if(answer.empty()) {
+            answer.push_back(n);
+            continue;
+        }
+        if(n>answer.back()) answer.push_back(n);
+        else {
+            int idx = lower_bound(answer.begin(), answer.end(), n) - answer.begin();
+            answer[idx] = n;
+        }
+    }
+}
+
+int main() {
+    cin>>N;
+    int a;
+    for(int i=0;i<N;i++) {
+        cin>>a;
+        arr.push_back(a);
+    }
+    LIS();
+    cout<<answer.size();
+}
