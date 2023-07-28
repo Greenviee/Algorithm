4b7751f1c9393014b803dbc4b85b47f98dcfa305
Jinwoo Kim <131782814+Greenviee@users.noreply.github.com>
2023-07-28 14:39:38 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int H, M;
+    scanf("%d %d", &H, &M);
+    if(M-45>=0)
+        printf("%d %d", H, M-45);
+    else
+        if(H==0)
+            printf("%d %d", 23, M+15);
+        else
+            printf("%d %d", H-1, M+15);
+        
+}
