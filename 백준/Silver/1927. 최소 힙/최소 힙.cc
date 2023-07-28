ea5bd37659b3079f72abfad4282b5cc5669032d7
Jinwoo Kim <131782814+Greenviee@users.noreply.github.com>
2023-07-28 12:59:04 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0); 
+    int N, x, k;
+    cin>>N;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(int i=0;i<N;i++) {
+        cin>>x;
+        if(x==0) {
+            if(pq.empty()) cout<<"0"<<"\n";
+            else {
+                k = pq.top();
+                pq.pop();
+                cout<<k<<"\n";
+            }
+        }
+        else {
+            pq.push(x);
+        }
+    }
+}
