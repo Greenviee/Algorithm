5294fc1d415c7a45d1da601635741b6f4c6be972
Jinwoo Kim <131782814+Greenviee@users.noreply.github.com>
2023-07-28 14:27:59 +0900

    [Bronze V] Title: 팩토리얼, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int factorial(int x)
+{
+    if(x==1||x==0)
+        return 1;
+    else
+        return x*factorial(x-1);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", factorial(n));
+}
