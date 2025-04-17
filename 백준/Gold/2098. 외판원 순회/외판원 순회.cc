7f2b7e0ada71584370d746a8d4aa2f6feee963fb
Jinwoo Kim <131782814+Greenviee@users.noreply.github.com>
2025-04-17 11:23:34 +0900

    [Gold I] Title: 외판원 순회, Time: 24 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.cc
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -1,39 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
-#include <cstring>
 #include <cmath>
+#include <cstring>
 using namespace std;
 
-const int INF = 987654321;
 int N;
-int cost[16][16], cache[16][1<<16];
+const int MAX_N = 16;
+const int INF = 987654321;
+int cost[MAX_N][MAX_N], cache[MAX_N][1 << MAX_N];
 
 int travel(int here, int visit) {
-    if((visit | ((1<<N)-1))==visit) {
-        if(cost[here][0]==0) return INF;
-        else return cost[here][0];
-    }
-    int& ret = cache[here][visit];
-    if(ret!=-1) return ret;
-    ret = INF;
-    
-    visit |= (1<<here);
-    int canVisit = ~visit;
-    for(int i=0;i<N;i++) {
-        if((canVisit & (1<<i)) && cost[here][i]!=0) 
-            ret = min(ret, cost[here][i] + travel(i, visit|(1<<i)));
-    }
-    return ret;
+	//모든 정점 방문 이후 시작점으로 복귀
+	if ((visit | ((1 << N) - 1)) == visit) {
+		if (cost[here][0] == 0) return INF;
+		return cost[here][0];
+	}
+	int& ret = cache[here][visit];
+	if (ret != -1) return ret;
+	ret = INF;
+	
+	visit |= (1 << here);
+	int canVisit = ~visit;
+	for (int i = 0; i < N; i++) {
+		if ((canVisit & (1 << i)) && cost[here][i] != 0) {
+			ret = min(ret, cost[here][i] + travel(i, visit | (1 << i)));
+		}
+	}
+	return ret;
 }
 
 int main() {
-    cin>>N;
-    for(int i=0;i<N;i++)
-        for(int j=0;j<N;j++)
-            cin>>cost[i][j];
-    memset(cache, -1, sizeof(cache));
-    int answer = travel(0, 0);
-    cout<<answer<<endl;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> cost[i][j];
+	memset(cache, -1, sizeof(cache));
+	cout << travel(0, 0);
 }
